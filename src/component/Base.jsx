946731e31c4b0329870d2597be7b50ad014de5be
tsumitakaoru <k.tsumita01@gmail.com>
2023-05-24 12:05:47 -0400

    Clearn code

--- src/component/Base.jsx
+++ src/component/Base.jsx
@@ -7,22 +7,22 @@ const Base = ({ setProfile} ) => {
       <h2>Base</h2>
       <button name="base" value="Hello">
         <img onClick={() => setProfile("base", "Jasmine")}
-        src="https://api.dicebear.com/6.x/adventurer/svg?seed=Jasmine&size=96&"
+        src="https://api.dicebear.com/6.x/adventurer/svg?seed=Jasmine&size=60&"
         alt="avatar-Jasmine" />
       </button>
       <button onClick={() => setProfile("base", "Milo")} name="base" value="Milo">
         <img
-        src="https://api.dicebear.com/6.x/adventurer/svg?seed=Milo&size=96&"
+        src="https://api.dicebear.com/6.x/adventurer/svg?seed=Milo&size=60&"
         alt="avatar-Milo" />
       </button>
       <button >
         <img onClick={() => setProfile("base", "Oliver")} name="base" value="Oliver"
-        src="https://api.dicebear.com/6.x/adventurer/svg?seed=Oliver&size=96&"
+        src="https://api.dicebear.com/6.x/adventurer/svg?seed=Oliver&size=60&"
         alt="avatar-Oliver" />
       </button>
       <button onClick={() => setProfile("base", "Jasper")} name="base" value="Jasper">
         <img
-        src="https://api.dicebear.com/6.x/adventurer/svg?seed=Jasper&size=96&"
+        src="https://api.dicebear.com/6.x/adventurer/svg?seed=Jasper&size=60&"
         alt="avatar-Jasper" />
       </button>
     </div>
